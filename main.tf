1aff417def4ed77c6a290c3b912773d3d6fcd018
RLangf <langfordrt@gmail.com>
2023-01-21 00:34:08 +0000

    Updated to add in site config version for NodeJs

--- main.tf
+++ main.tf
@@ -28,22 +28,27 @@ module "storage" {
 
 // Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
-  name = "webplan-foundryvtt"
+  name = "webplan-${terraform.workspace}"
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
   os_type = "Linux"
-  sku_name = "B1"
+  sku_name = "F1"
 }
 
 // Create the web app 
 resource "azurerm_linux_web_app" "webapp" {
-  name = "webapp-foundryvtt"
+  name = "webapp-${terraform.workspace}"
   resource_group_name = azurerm_resource_group.resource_group.name
   location = azurerm_resource_group.resource_group.location
   service_plan_id = azurerm_service_plan.appserviceplan.id
   https_only = false
+  // Sets up app environment
   site_config {
     minimum_tls_version = "1.2"
+    linux_fx_version = "NODE|18-lts"
+  }
+  appsettings {
+    license = ""
   }
 }
 
